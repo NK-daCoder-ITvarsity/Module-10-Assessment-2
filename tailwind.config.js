076724f276c86443de2cf28d05e185595d357daa
NK-daCoder <gpnkstudios@gmail.com>
2025-03-04 16:58:16 +0200

    frontend 0.0 setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
  /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./*.{html,js,jsx,ts,tsx}", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        spin360: "spin360 1s linear infinite",
+      },
+      keyframes: {
+        spin360: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+    },
   },
   plugins: [],
-}
+}}
