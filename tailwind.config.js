a7388934da2b651041d93292cf0fca0fe0c81cd3
NK-daCoder <gpnkstudios@gmail.com>
2025-03-06 16:57:44 +0200

    simpler advanced recipe book app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js,jsx,ts,tsx}", "./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      animation: {
-        spin360: "spin360 1s linear infinite",
-      },
-      keyframes: {
-        spin360: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" },
-        },
-      },
-  },
-  plugins: [],
-}}
+  content: ["./index.html" ,"./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+}
+
